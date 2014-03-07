fd2b22d7dfb13ff63f47658c94f3b58928e1dcfc
dwong229 <denwong@seas.upenn.edu>
2014-03-06 19:19:23 -0500

    Update slider

--- mbrAnalytics/thetaPlotSlider_Callback.m
+++ mbrAnalytics/thetaPlotSlider_Callback.m
@@ -1,6 +1,11 @@
 function thetaPlotSlider_Callback(hObject,event,pplot,qplot,varargin)
 
-% take slider value for p and update theta plot
+% take slider value for p and update htheta plot location of circle
+% INPUT
+% hObject: plot3(x,y,dthdt), circle around point (x,y,dthdt)
+% pplot,qplot,dthdt 
+% 
+
 
 slider_value = round(get(hObject,'Value'));
 
